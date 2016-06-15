e39de353bbcb772226884bc19161e26e71ed3ba2
Adam Michael <adam@ajmichael.net>
2016-06-15 01:36:24 -0500

    Completed problem 66.

---
+++ src/main/java/net/ajmichael/projecteuler/Problem066.java
@@ -0,0 +1,61 @@
+package net.ajmichael.projecteuler;
+
+import net.ajmichael.util.Pair;
+
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Problem066 {
+  public static void main(String args[]) {
+    System.out.println(solve());
+  }
+
+  public static long solve() {
+    Set<Integer> squares = new HashSet<>();
+    for (int i = 1; i <= 1000; i++) {
+      squares.add(i*i);
+    }
+    int bestD = 0;
+    BigInteger largestX = BigInteger.ZERO;
+    for (int D = 2; D <= 1000; D++) {
+      if (!squares.contains(D)) {
+        BigInteger minX = getMinX(D);
+        if (minX.compareTo(largestX) > 0) {
+          bestD = D;
+          largestX = minX;
+        }
+      }
+    }
+    return bestD;
+  }
+
+  private static BigInteger getMinX(int n) {
+    BigInteger N = BigInteger.valueOf(n);
+    BigInteger a0 = BigInteger.valueOf((long) Math.sqrt(n));
+    Pair<BigInteger> continuants = new Pair<>(a0, BigInteger.ONE);
+    Pair<BigInteger> oldContinuants = new Pair<>(BigInteger.ONE, BigInteger.ZERO);
+    Pair<BigInteger> current = new Pair<>(BigInteger.ONE, a0);
+    while (true) {
+      if (continuants.x.multiply(continuants.x).subtract(continuants.y.multiply(continuants.y).multiply(N)).intValue() == 1) {
+        return continuants.x;
+      }
+      BigInteger denom = N.subtract(current.y.multiply(current.y)).divide(current.x);
+      BigInteger temp = current.y;
+      long bn = -1;
+      while (temp.add(a0).compareTo(BigInteger.ZERO) >= 0) {
+        temp = temp.subtract(denom);
+        bn++;
+      }
+      temp = temp.add(denom);
+      current = new Pair<>(denom, temp.negate());
+
+      Pair<BigInteger> tempContinuant = continuants;
+      continuants = new Pair<>(
+          BigInteger.valueOf(bn).multiply(continuants.x).add(oldContinuants.x),
+          BigInteger.valueOf(bn).multiply(continuants.y).add(oldContinuants.y)
+      );
+      oldContinuants = tempContinuant;
+    }
+  }
+}
