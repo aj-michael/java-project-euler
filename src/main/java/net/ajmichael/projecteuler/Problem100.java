cf5f1794a9fe6e50c4c986706a49f5e13370ede7
Adam Michael <adam@ajmichael.net>
2016-06-27 22:03:38 -0500

    Completed problem 100.

---
+++ src/main/java/net/ajmichael/projecteuler/Problem100.java
@@ -0,0 +1,21 @@
+package net.ajmichael.projecteuler;
+
+public class Problem100 {
+  public static void main(String args[]) {
+    System.out.println(solve());
+  }
+
+  /** Used https://www.alpertron.com.ar/QUAD.HTM to get recurrence. */
+  public static long solve() {
+    long b = 15;
+    long n = 21;
+    while (true) {
+      if (n > 1000000000000L) {
+        return b;
+      }
+      long oldB = b;
+      b = 3 * b + 2 * n - 2;
+      n = 4 * oldB + 3 * n - 3;
+    }
+  }
+}
